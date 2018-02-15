dc7728ef8ac2664440169e6fdb9de7a86067b1c6
Adarsh Agnihotri <Adu143@users.noreply.github.com>
2018-02-16 00:06:34 +0100

    Update SvoLiveSvo.sh

--- script-config-launch/SvoLiveSvo.sh
+++ script-config-launch/SvoLiveSvo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #Setting up ROS and Exporting the core
 source catkin_ws/devel/setup.bash
-export ROS_MASTER_URI=http://192.168.147.232:11311
-export ROS_IP=192.168.147.232 
+export ROS_MASTER_URI=http://192.168.147.232:11311 #use odroid's IP or hostname here
+export ROS_IP=192.168.147.232 #Odroid's IP
 
 sleep 5 #delay of 5 sec to allow usb_cam to run before svo launch
 
