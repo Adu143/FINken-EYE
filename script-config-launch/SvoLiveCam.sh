e9b0659a2c360f27fd4696543797da367be08ed6
Adarsh Agnihotri <Adu143@users.noreply.github.com>
2018-02-16 00:05:31 +0100

    Delete SvoLiveCam.sh

--- script-config-launch/SvoLiveCam.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#Setting up ROS and Exporting the core
-source catkin_ws/devel/setup.bash
-export ROS_MASTER_URI=http://192.168.147.232:11311
-export ROS_IP=192.168.147.232
-
-sleep 5
-roslaunch usb_cam usb_cam-test.launch
