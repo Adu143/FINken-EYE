3c20162c47541b8ea69e94e133b4e28cd4fd8e99
Adarsh Agnihotri <Adu143@users.noreply.github.com>
2018-02-16 00:04:08 +0100

    Update SvoLiveSvo.sh

--- script-config-launch/SvoLiveSvo.sh
+++ script-config-launch/SvoLiveSvo.sh
@@ -4,4 +4,6 @@ source catkin_ws/devel/setup.bash
 export ROS_MASTER_URI=http://192.168.147.232:11311
 export ROS_IP=192.168.147.232 
 
+sleep 5 #delay of 5 sec to allow usb_cam to run before svo launch
+
 roslaunch svo_ros test.launch
